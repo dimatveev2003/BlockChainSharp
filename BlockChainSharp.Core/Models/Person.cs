443b84563097ef44d22e60975f2381d32192aa27
Дмитрий Матвеев <dimamatveev200398@gmail.com>
2019-12-08 21:10:45 +0300

    Добавил количество валюты на счете и операции с ними

--- BlockChainSharp.Core/Models/Person.cs
+++ BlockChainSharp.Core/Models/Person.cs
@@ -8,14 +8,32 @@ namespace BlockChainSharp.Core.Models
 {
     public class Person
     {
+        private double money;
         public string Name { get; set; }
         public string Surname { get; set; }
         public string AccountNumber { get; set; }
         public string Password { get; set; }
+        public double Money => money;
+
+        public Person()
+        {
+            //Количество валюты при регистрации
+            money = 10;
+        }
+
+        public void IncreaseMoney(double count)
+        {
+            money += count;
+        }
+
+        public void DecreaseMoney(double count)
+        {
+            money -= count;
+        }
 
         public override string ToString()
         {
-            return $"({Name}:{Surname}:{AccountNumber}:{Password})";
+            return $"({Name}:{Surname}:{AccountNumber})";
         }
     }
 }
